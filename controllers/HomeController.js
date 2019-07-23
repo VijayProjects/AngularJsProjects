ca3aafce9a62df26f3747a6c4118f06d8ae9bd1e
vijay <vijaybabu.k007@gmail.com>
2019-07-23 19:22:26 +0530

    ngModel directive demo added

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -2,9 +2,11 @@ app.controller("HomeController",function($scope) {
     $scope.title = 'Product Details';
     $scope.products = [
         {Id:1001, Name: 'Samsung Tv', Price: 43550.99},
-        {Id:1002, Name: 'Mobile', Price: 13990.99}
+        {Id:1002, Name: 'Mobile', Price: 13990.99},
+        {Id:1003, Name: 'Laptop', Price: 13990.99}
     ];
     $scope.category = function() {
         return 'Eletronics';
     };
 });
+
