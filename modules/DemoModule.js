65bd7c4a84fefe6418921d580c636bb868f7b695
vijay <vijaybabu.k007@gmail.com>
2019-08-22 19:57:21 +0530

    routeProvider added

--- modules/DemoModule.js
+++ modules/DemoModule.js
@@ -1,1 +1,24 @@
-var app = angular.module("DemoModule",[]);
+var app = angular.module("DemoApp",['ngRoute']);
+//URL's configuration
+app.config(function($routeProvider) {
+  $routeProvider
+  .when("/", {
+    templateUrl: 'views/home.html',
+    controller: 'HomeController'
+  })
+  .when("/home", {
+    templateUrl: 'views/home.html',
+    controller: 'HomeController'
+  })
+  .when("/about", {
+    templateUrl: 'views/about.html',
+    controller: 'AboutController'
+  })
+  .when("/contact", {
+    templateUrl: 'views/contact.html',
+    controller: 'ContactController'
+  })
+  .otherwise({
+    template:"<h2 style='color:red'>Page is not found</h2>"
+  });
+});
